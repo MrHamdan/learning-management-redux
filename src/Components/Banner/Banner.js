1b4b3d6fe636d7b0dbee0908a1ce93c09b5cf1cf
Staff Asia <staffasia@Staffs-iMac.local>
2022-02-08 18:14:56 +0600

    adding banner image and button

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,4 +1,5 @@
-import { Container } from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
 import bannerImage from '../../Images/Banner.png';
 
@@ -20,13 +21,28 @@ const Banner = () => {
             fontFamily: 'Inter',
             fontSize: '14px',
             fontWeight: '900',
+            '&:before': {
+                content: '""',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0,0,0,0.2)',
+            },
         }
     }
 
     return (
         <div style={Styles.banner}>
             <Container>
-
+                <Box><Typography sx={{ fontSize: '80px', fontWeight: '900', color: 'white', marginBottom: '20px' }}>
+                    ENTER THE ERA OF <br /> APPX LEARNING
+                </Typography>
+                    <Typography variant="h6" sx={{ fontSize: '28px', fontWeight: '400', color: 'white' }}>
+                        Learn Wherever and When you Want Without Limits
+                    </Typography></Box>
+                <Box sx={{ textAlign: 'center', marginTop: '30px' }}><Button variant="contained" sx={{ padding: '10px 40px', backgroundColor: '#0D2A62 !important' }} >Start Learning</Button></Box>
             </Container>
         </div>
     );
