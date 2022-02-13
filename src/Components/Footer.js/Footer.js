a34c9b1ee8fa8592bc861166a81b15d7992bb9b4
MrHamdan <mr.hamdanahmed@gmail.com>
2022-02-13 22:26:18 +0600

    adding footer file

--- src/Components/Footer.js/Footer.js
+++ src/Components/Footer.js/Footer.js
@@ -1,11 +1,35 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Grid, Paper, Typography } from '@mui/material';
 import React from 'react';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 const Footer = () => {
     return (
         <Box>
             <Container>
-                <Typography>This is footer</Typography>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={8} xl={3}>
+                            <Item>xs=8</Item>
+                        </Grid>
+                        <Grid item xs={4} xl={3}>
+                            <Item>xs=4</Item>
+                        </Grid>
+                        <Grid item xs={4} xl={3}>
+                            <Item>xs=4</Item>
+                        </Grid>
+                        <Grid item xs={8} xl={3}>
+                            <Item>xs=8</Item>
+                        </Grid>
+                    </Grid>
+                </Box>
             </Container>
         </Box>
     );
