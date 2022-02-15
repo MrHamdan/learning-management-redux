6387ae8904e588a3916b61f3eaf93142f63560b2
Staff Asia <staffasia@Staffs-iMac.local>
2022-02-15 18:03:36 +0600

    updating

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import { Box } from '@mui/material';
+
+const Cart = () => {
+    return (
+        <Box>
+            <Header />
+            
+            <Footer />   
+        </Box>
+    );
+};
+
+export default Cart;
