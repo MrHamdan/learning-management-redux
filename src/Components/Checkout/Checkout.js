d99ebac3cd969dd3d7013538dc9ac7d43d267b76
MrHamdan <mr.hamdanahmed@gmail.com>
2022-02-21 15:00:06 +0600

    your order table added

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import { Box, FormControl, Grid, InputLabel, MenuItem, Paper, Select, TextField, Typography } from '@mui/material';
+import { Box, FormControl, Grid, InputLabel, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material';
 import React from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
@@ -23,6 +23,36 @@ const Item = styled(Paper)(({ theme }) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
 }));
+
+
+const TAX_RATE = 0.07;
+
+function ccyFormat(num) {
+    return `${num.toFixed(2)}`;
+}
+
+function priceRow(qty, unit) {
+    return qty * unit;
+}
+
+function createRow(desc, qty, unit) {
+    const price = priceRow(qty, unit);
+    return { desc, qty, unit, price };
+}
+
+function subtotal(items) {
+    return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
+}
+
+const rows = [
+    createRow('Paperclips (Box)', 100, 1.15),
+    createRow('Paper (Case)', 10, 45.99),
+    createRow('Waste Basket', 2, 17.99),
+];
+
+const invoiceSubtotal = subtotal(rows);
+const invoiceTaxes = TAX_RATE * invoiceSubtotal;
+const invoiceTotal = invoiceTaxes + invoiceSubtotal;
 
 
 
@@ -85,39 +115,54 @@ const Checkout = () => {
                     <Box sx={{ width: '1170px', height: '455px', backgroundColor: 'white', marginTop: '120px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <Box sx={{ flexGrow: 1 }}>
                             <Grid container spacing={2}>
-                                <Grid item xs={8} xl={6}>
+                                <Grid item xs={8} xl={12}>
                                     <Item sx={{ textAlign: 'left' }}><Typography sx={{ fontFamily: 'Inter', fontWeight: 'bold', fontSize: '36px', marginBottom: '30px', color: '#201E1E' }}>
                                         Your Order
                                     </Typography>
-                                        <Typography >First Name</Typography>
+                                        {/* <Typography >First Name</Typography>
                                         <TextField sx={{ marginBottom: '20px' }} fullWidth id="fullWidth" />
                                         <Typography>Address</Typography>
                                         <TextField sx={{ marginBottom: '20px' }} fullWidth id="fullWidth" />
                                         <Typography>Phone</Typography>
-                                        <TextField fullWidth id="fullWidth" />
+                                        <TextField fullWidth id="fullWidth" /> */}
+                                        <TableContainer component={Paper}>
+                                            <Table sx={{ minWidth: 700 }} aria-label="spanning table">
+                                                <TableHead>
+                                                    <TableRow>
+                                                        <TableCell align="left" colSpan={3}>
+                                                            Course Name
+                                                        </TableCell>
+                                                        <TableCell align="right">Subtotal</TableCell>
+                                                    </TableRow>
+                                                </TableHead>
+                                                <TableBody>
+                                                    {rows.map((row) => (
+                                                        <TableRow key={row.desc}>
+                                                            <TableCell>{row.desc}</TableCell>
+                                                            <TableCell align="right"></TableCell>
+                                                            <TableCell align="right"></TableCell>
+                                                            <TableCell align="right">{ccyFormat(row.price)}</TableCell>
+                                                        </TableRow>
+                                                    ))}
+
+                                                    <TableRow>
+                                                        <TableCell rowSpan={3} />
+                                                        <TableCell colSpan={2}>Subtotal</TableCell>
+                                                        <TableCell align="right">{ccyFormat(invoiceSubtotal)}</TableCell>
+                                                    </TableRow>
+                                                    <TableRow>
+                                                        <TableCell>Tax</TableCell>
+                                                        <TableCell align="right"></TableCell>
+                                                        <TableCell align="right">{ccyFormat(invoiceTaxes)}</TableCell>
+                                                    </TableRow>
+                                                    <TableRow>
+                                                        <TableCell colSpan={2}>Total</TableCell>
+                                                        <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
+                                                    </TableRow>
+                                                </TableBody>
+                                            </Table>
+                                        </TableContainer>
                                     </Item>
-                                </Grid>
-                                <Grid item xs={4} xl={6}>
-                                    <Item sx={{ textAlign: 'left' }}>
-                                        <Typography sx={{ marginTop: '85px' }}>Last Name</Typography>
-                                        <TextField sx={{ marginBottom: '20px' }} fullWidth id="fullWidth" />
-                                        <Typography>Country / Region</Typography>
-                                        <FormControl fullWidth sx={{ marginBottom: '20px' }}>
-                                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                                            <Select
-                                                labelId="demo-simple-select-label"
-                                                id="demo-simple-select"
-                                                value={age}
-                                                label="Age"
-                                                onChange={handleChange}
-                                            >
-                                                <MenuItem value={10}>Ten</MenuItem>
-                                                <MenuItem value={20}>Twenty</MenuItem>
-                                                <MenuItem value={30}>Thirty</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                        <Typography>Email Address</Typography>
-                                        <TextField fullWidth id="fullWidth" /></Item>
                                 </Grid>
                             </Grid>
                         </Box>
