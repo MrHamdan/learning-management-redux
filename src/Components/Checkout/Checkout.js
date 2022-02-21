b7ad685b5d40dda46582066eb20c2f30d751a2c3
MrHamdan <mr.hamdanahmed@gmail.com>
2022-02-21 14:49:29 +0600

    checkout page more detail added

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Paper, TextField, Typography } from '@mui/material';
+import { Box, FormControl, Grid, InputLabel, MenuItem, Paper, Select, TextField, Typography } from '@mui/material';
 import React from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
@@ -27,6 +27,14 @@ const Item = styled(Paper)(({ theme }) => ({
 
 
 const Checkout = () => {
+
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+
     return (
         <Box>
             <Header color='white' />
@@ -49,10 +57,107 @@ const Checkout = () => {
                                 </Grid>
                                 <Grid item xs={4} xl={6}>
                                     <Item sx={{ textAlign: 'left' }}>
-                                        <Typography sx={{marginTop:'85px'}}>Last Name</Typography>
+                                        <Typography sx={{ marginTop: '85px' }}>Last Name</Typography>
                                         <TextField sx={{ marginBottom: '20px' }} fullWidth id="fullWidth" />
                                         <Typography>Country / Region</Typography>
+                                        <FormControl fullWidth sx={{ marginBottom: '20px' }}>
+                                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                                value={age}
+                                                label="Age"
+                                                onChange={handleChange}
+                                            >
+                                                <MenuItem value={10}>Ten</MenuItem>
+                                                <MenuItem value={20}>Twenty</MenuItem>
+                                                <MenuItem value={30}>Thirty</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                        <Typography>Email Address</Typography>
+                                        <TextField fullWidth id="fullWidth" /></Item>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <Box sx={{ width: '1170px', height: '455px', backgroundColor: 'white', marginTop: '120px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Box sx={{ flexGrow: 1 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={8} xl={6}>
+                                    <Item sx={{ textAlign: 'left' }}><Typography sx={{ fontFamily: 'Inter', fontWeight: 'bold', fontSize: '36px', marginBottom: '30px', color: '#201E1E' }}>
+                                        Your Order
+                                    </Typography>
+                                        <Typography >First Name</Typography>
                                         <TextField sx={{ marginBottom: '20px' }} fullWidth id="fullWidth" />
+                                        <Typography>Address</Typography>
+                                        <TextField sx={{ marginBottom: '20px' }} fullWidth id="fullWidth" />
+                                        <Typography>Phone</Typography>
+                                        <TextField fullWidth id="fullWidth" />
+                                    </Item>
+                                </Grid>
+                                <Grid item xs={4} xl={6}>
+                                    <Item sx={{ textAlign: 'left' }}>
+                                        <Typography sx={{ marginTop: '85px' }}>Last Name</Typography>
+                                        <TextField sx={{ marginBottom: '20px' }} fullWidth id="fullWidth" />
+                                        <Typography>Country / Region</Typography>
+                                        <FormControl fullWidth sx={{ marginBottom: '20px' }}>
+                                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                                value={age}
+                                                label="Age"
+                                                onChange={handleChange}
+                                            >
+                                                <MenuItem value={10}>Ten</MenuItem>
+                                                <MenuItem value={20}>Twenty</MenuItem>
+                                                <MenuItem value={30}>Thirty</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                        <Typography>Email Address</Typography>
+                                        <TextField fullWidth id="fullWidth" /></Item>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <Box sx={{ width: '1170px', height: '455px', backgroundColor: 'white', marginTop: '120px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Box sx={{ flexGrow: 1 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={8} xl={6}>
+                                    <Item sx={{ textAlign: 'left' }}><Typography sx={{ fontFamily: 'Inter', fontWeight: 'bold', fontSize: '36px', marginBottom: '30px', color: '#201E1E' }}>
+                                        Payment Method
+                                    </Typography>
+                                        <Typography >First Name</Typography>
+                                        <TextField sx={{ marginBottom: '20px' }} fullWidth id="fullWidth" />
+                                        <Typography>Address</Typography>
+                                        <TextField sx={{ marginBottom: '20px' }} fullWidth id="fullWidth" />
+                                        <Typography>Phone</Typography>
+                                        <TextField fullWidth id="fullWidth" />
+                                    </Item>
+                                </Grid>
+                                <Grid item xs={4} xl={6}>
+                                    <Item sx={{ textAlign: 'left' }}>
+                                        <Typography sx={{ marginTop: '85px' }}>Last Name</Typography>
+                                        <TextField sx={{ marginBottom: '20px' }} fullWidth id="fullWidth" />
+                                        <Typography>Country / Region</Typography>
+                                        <FormControl fullWidth sx={{ marginBottom: '20px' }}>
+                                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                                value={age}
+                                                label="Age"
+                                                onChange={handleChange}
+                                            >
+                                                <MenuItem value={10}>Ten</MenuItem>
+                                                <MenuItem value={20}>Twenty</MenuItem>
+                                                <MenuItem value={30}>Thirty</MenuItem>
+                                            </Select>
+                                        </FormControl>
                                         <Typography>Email Address</Typography>
                                         <TextField fullWidth id="fullWidth" /></Item>
                                 </Grid>
